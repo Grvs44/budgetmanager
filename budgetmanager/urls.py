61e8e457c536aebaa3e6432874f6b16067c5a6e7
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-07-20 15:46:12 +0100

    Move knox from app to project

--- budgetmanager/urls.py
+++ budgetmanager/urls.py
@@ -1,19 +1,13 @@
 '''
 URL configuration for budgetmanager app
 '''
-from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 from . import views
 
-app_name = 'clotheswear' # pylint: disable=invalid-name
+app_name = 'clotheswear'  # pylint: disable=invalid-name
 router = DefaultRouter()
 router.register(r'budget', views.BudgetViewSet)
 router.register(r'payee', views.PayeeViewSet)
 router.register(r'payment', views.PaymentViewSet)
 
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('api/', include(router.urls)),
-    path('api/', include('knox.urls')),
-]
+urlpatterns = router.urls
--- budgetmanager/views.py
+++ budgetmanager/views.py
@@ -1,7 +1,4 @@
 # pylint: disable=no-member
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views import View
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from . import (
@@ -10,18 +7,6 @@ from . import (
     permissions,
     serializers,
 )
-
-
-class HomeView(LoginRequiredMixin, View):
-    """
-    The view for the home page
-    """
-
-    def get(self, request):
-        """
-        Handle the GET request for the home page
-        """
-        return render(request, 'clotheswear/home.html')
 
 
 class BaseViewSet(ModelViewSet):
@@ -36,9 +21,11 @@ class BudgetViewSet(BaseViewSet):
     queryset = models.Budget.objects.all()
     serializer_class = serializers.BudgetSerializer
 
+
 class PayeeViewSet(BaseViewSet):
     queryset = models.Payee.objects.all()
     serializer_class = serializers.PayeeSerializer
+
 
 class PaymentViewSet(BaseViewSet):
     queryset = models.Payment.objects.all()
