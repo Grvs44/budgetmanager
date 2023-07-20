e7258ec2cb533b8d80c379067838df2a8082a90c
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-07-20 15:59:47 +0100

    Fix API views and serializers and add 'add_from_csv' action for budgets

--- budgetmanager/serializers.py
+++ budgetmanager/serializers.py
@@ -1,21 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    HiddenField,
+    CurrentUserDefault
+)
+
 from . import models
 
 
-class BudgetSerializer(ModelSerializer):
+class BaseSerializer(ModelSerializer):
+    user = HiddenField(default=CurrentUserDefault())
+
+
+class BudgetSerializer(BaseSerializer):
     class Meta:
         model = models.Budget
-        fields = ['id', 'name', 'description', 'active']
+        fields = ['id', 'name', 'description', 'active', 'user']
 
 
-class PayeeSerializer(ModelSerializer):
+class PayeeSerializer(BaseSerializer):
     class Meta:
         model = models.Payee
-        fields = ['id', 'name', 'description', 'active']
+        fields = ['id', 'name', 'description', 'user']
 
 
-class PaymentSerializer(ModelSerializer):
+class PaymentSerializer(BaseSerializer):
     class Meta:
         model = models.Payment
-        fields = ['id', 'name', 'description',
-                  'payee', 'budget', 'amount', 'date']
+        fields = ['id', 'notes', 'payee', 'budget',
+                  'amount', 'date', 'pending', 'user']
--- budgetmanager/views.py
+++ budgetmanager/views.py
@@ -1,5 +1,8 @@
 # pylint: disable=no-member
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from . import (
     filters,
@@ -13,13 +16,15 @@ class BaseViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated, permissions.IsOwner]
     filter_backends = [filters.OwnerFilter]
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
 
 class BudgetViewSet(BaseViewSet):
     queryset = models.Budget.objects.all()
     serializer_class = serializers.BudgetSerializer
+
+    @action(methods=['POST'], detail=True, url_path='csv')
+    def add_from_csv(self, request, pk):
+        self.get_object().add_from_csv(request.data['csv'])
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
 class PayeeViewSet(BaseViewSet):
