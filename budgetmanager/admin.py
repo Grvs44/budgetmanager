d525c085cd0ac11585f8ecece86056fc8e4c4920
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-08-04 16:55:04 +0100

    Choose user for new object, but make read-only for existing objects

--- budgetmanager/admin.py
+++ budgetmanager/admin.py
@@ -3,42 +3,49 @@ from django.contrib import admin
 from . import models
 
 
+class BaseAdmin(admin.ModelAdmin):
+    readonly_fields = ('user',)
+    list_per_page = 20
+
+    def get_readonly_fields(self, request, obj=...):
+        if obj is None:
+            return tuple()
+        return self.readonly_fields
+
+    class Meta:
+        abstract = True
+
+
 @admin.register(models.Budget)
-class BudgetAdmin(admin.ModelAdmin):
+class BudgetAdmin(BaseAdmin):
     '''Settings for the Budget admin'''
     list_display = ('user', 'name', 'active')
     list_display_links = list_display
     list_filter = ('user', 'active')
-    readonly_fields = ('user',)
     sortable_by = list_display
     search_fields = ('name',)
     search_help_text = 'Search by budget name'
-    list_per_page = 20
 
 
 @admin.register(models.Payee)
-class PayeeAdmin(admin.ModelAdmin):
+class PayeeAdmin(BaseAdmin):
     '''Settings form the Payee admin'''
     list_display = ('user', 'name')
     list_display_links = list_display
     list_filter = ('user',)
-    readonly_fields = ('user',)
     sortable_by = list_display
     search_fields = ('name',)
     search_help_text = 'Search by payee name'
-    list_per_page = 20
 
 
 @admin.register(models.Payment)
-class PaymentAdmin(admin.ModelAdmin):
+class PaymentAdmin(BaseAdmin):
     '''Settings for the Payment admin'''
     date_hierarchy = 'date'
     list_display = ('user', 'budget', 'payee', 'date')
     list_display_links = list_display
     list_filter = ('user', 'budget', 'payee', 'date')
-    readonly_fields = ('user',)
     sortable_by = list_display
-    list_per_page = 20
 
     def get_form(self, request, obj=..., change=..., **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
