e3c5e8b1c6f9767ddbd76653b28ae862fe0c5be8
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-06-30 16:56:22 +0100

    Update budget Invalidating the cached items doesn't clear the displayed list

--- budgetmanagerpwa/src/redux/apiSlice.js
+++ budgetmanagerpwa/src/redux/apiSlice.js
@@ -1,11 +1,14 @@
 import Cookies from 'js-cookie'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+const headers = { 'X-CSRFToken': Cookies.get('csrftoken') }
 // From https://codesandbox.io/s/react-rtk-query-inifinite-scroll-8kj9bh
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/budgetmanager/api/' }),
+  tagTypes: ['Budget'],
   endpoints: (builder) => ({
     getBudgets: builder.query({
       query: (page = 0) => `budget/?offset=${page * 10}&limit=10`,
+      providesTags: ['Budget'],
       serializeQueryArgs: ({ endpointName }) => {
         return endpointName
       },
@@ -26,18 +29,29 @@ export const apiSlice = createApi({
         url: 'budget/',
         method: 'POST',
         body,
-        headers:{'X-CSRFToken': Cookies.get('csrftoken'),}
+        headers,
       }),
-      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+      invalidatesTags: ['Budget'],
+    }),
+    updateBudget: builder.mutation({
+      query: (body) => ({
+        url: `budget/${body.id}/`,
+        method: 'PATCH',
+        body,
+        headers,
+      }),
+      async onQueryStarted({ id }, { dispatch, queryFulfilled }) {
         try {
           const query = await queryFulfilled
           dispatch(
-            apiSlice.util.updateQueryData('getBudgets', undefined, (draft) => {
+            apiSlice.util.updateQueryData('getBudget', undefined, (draft) => {
               draft.results.push(query.data)
+              draft.results[draft.results.indexOf((e) => e.id == id)] =
+                query.data
             })
           )
         } catch {
-          console.log('useCreateBudgetMutation error')
+          console.log('useUpdateBudgetMutation error')
         }
       },
     }),
@@ -48,4 +62,5 @@ export const {
   useGetBudgetsQuery,
   useGetBudgetQuery,
   useCreateBudgetMutation,
+  useUpdateBudgetMutation,
 } = apiSlice
