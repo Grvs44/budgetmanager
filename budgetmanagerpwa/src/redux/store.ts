b833fba2fd4c32dd194caecb263f7b401d45b756
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-06-30 21:31:10 +0100

    Remove budgetSlice

--- budgetmanagerpwa/src/redux/store.ts
+++ budgetmanagerpwa/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import budgetReducer from './budgetSlice'
 import titleReducer from './titleSlice'
 import userReducer from './userSlice'
 import { apiSlice } from './apiSlice'
 
 export default configureStore({
   reducer: {
-    budget: budgetReducer,
     title: titleReducer,
     user: userReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
