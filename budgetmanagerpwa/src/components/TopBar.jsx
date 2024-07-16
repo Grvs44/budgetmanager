a043aa813f95e4edc57748b7385e55d98f16de03
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-07-16 15:03:33 +0100

    Add slashes to drawer links

--- budgetmanagerpwa/src/components/TopBar.jsx
+++ budgetmanagerpwa/src/components/TopBar.jsx
@@ -44,21 +44,21 @@ export default function TopBar({ user, title }) {
               </ListItemButton>
             </ListItem>
           </Link>
-          <Link to={`${rootPath}budget`}>
+          <Link to={`${rootPath}/budget`}>
             <ListItem>
               <ListItemButton>
                 <ListItemText>Budgets</ListItemText>
               </ListItemButton>
             </ListItem>
           </Link>
-          <Link to={`${rootPath}payee`}>
+          <Link to={`${rootPath}/payee`}>
             <ListItem>
               <ListItemButton>
                 <ListItemText>Payees</ListItemText>
               </ListItemButton>
             </ListItem>
           </Link>
-          <Link to={`${rootPath}payment`}>
+          <Link to={`${rootPath}/payment`}>
             <ListItem>
               <ListItemButton>
                 <ListItemText>Payments</ListItemText>
