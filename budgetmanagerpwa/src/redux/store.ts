3b686a27d007ce36d4810d97adb1c1d1328e1814
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-07-16 16:12:35 +0100

    Remove userSlice

--- budgetmanagerpwa/src/redux/store.ts
+++ budgetmanagerpwa/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import titleReducer from './titleSlice'
-import userReducer from './userSlice'
 import { apiSlice } from './apiSlice'
 
 export default configureStore({
   reducer: {
     title: titleReducer,
-    user: userReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
