c81678c1dcab5e69d2080b43cbc64aa90aaa4d44
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-07-23 18:30:28 +0100

    Delete src/api

--- budgetmanagerpwa/src/pages/JoinForm.jsx
+++ budgetmanagerpwa/src/pages/JoinForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { join } from '../api/join'
 import { useJoinBudgetMutation } from '../redux/apiSlice'
 
 export default function JoinForm() {
