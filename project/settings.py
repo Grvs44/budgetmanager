11ce35693b9b28184bc8d2957048b6b5fae686a9
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-08-19 17:47:49 +0100

    Fix CSRF_TRUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000/', 'http://localhost:3000/']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000']
