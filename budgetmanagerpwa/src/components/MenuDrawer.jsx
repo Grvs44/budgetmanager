c76bcdeefcc50c9f158d0dfa131c542d2c9d5fe8
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-08-24 16:07:44 +0100

    Fix how links are displayed

--- budgetmanagerpwa/src/components/MenuDrawer.jsx
+++ budgetmanagerpwa/src/components/MenuDrawer.jsx
@@ -4,52 +4,43 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
-import { Link } from 'react-router-dom'
+import ListItemButtonLink from './ListItemButtonLink'
 import { ListItemIcon } from '@mui/material'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import InstallPwaListItem from './InstallPwaListItem'
 
 export default function MenuDrawer({ open, onClose, user }) {
-  const linkTo = (...paths) => import.meta.env.BASE_URL.concat(...paths)
-
   return (
     <Drawer anchor="left" open={open} onClose={onClose}>
       <List onClick={onClose}>
-        <Link to={linkTo()}>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText>Home</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </Link>
-        <Link to={linkTo('budget')}>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText>Budgets</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </Link>
-        <Link to={linkTo('payee')}>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText>Payees</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </Link>
-        <Link to={linkTo('payment')}>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText>Payments</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </Link>
-        <InstallPwaListItem/>
+        <ListItem>
+          <ListItemButtonLink to="">
+            <ListItemText>Home</ListItemText>
+          </ListItemButtonLink>
+        </ListItem>
+        <ListItem>
+          <ListItemButtonLink to="budget">
+            <ListItemText>Budgets</ListItemText>
+          </ListItemButtonLink>
+        </ListItem>
+        <ListItem>
+          <ListItemButtonLink to="payee">
+            <ListItemText>Payees</ListItemText>
+          </ListItemButtonLink>
+        </ListItem>
+        <ListItem>
+          <ListItemButtonLink to="payment">
+            <ListItemText>Payments</ListItemText>
+          </ListItemButtonLink>
+        </ListItem>
+        <InstallPwaListItem />
         <ListItem>
           <ListItemButton>
-          <ListItemIcon>
-            <AccountCircleIcon/>
-          </ListItemIcon>
-          <ListItemText>{user.username}</ListItemText></ListItemButton>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText>{user.username}</ListItemText>
+          </ListItemButton>
         </ListItem>
       </List>
     </Drawer>
