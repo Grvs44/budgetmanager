b803d6da4ce90d5832559c70f5fe52ae471d708f
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2024-08-24 16:12:29 +0100

    Added icons

--- budgetmanagerpwa/src/components/MenuDrawer.jsx
+++ budgetmanagerpwa/src/components/MenuDrawer.jsx
@@ -7,6 +7,10 @@ import ListItemText from '@mui/material/ListItemText'
 import ListItemButtonLink from './ListItemButtonLink'
 import { ListItemIcon } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import HomeIcon from '@mui/icons-material/Home'
+import SavingsIcon from '@mui/icons-material/Savings'
+import StoreIcon from '@mui/icons-material/Store'
+import PaymentsIcon from '@mui/icons-material/Payments'
 import InstallPwaListItem from './InstallPwaListItem'
 
 export default function MenuDrawer({ open, onClose, user }) {
@@ -15,21 +19,33 @@ export default function MenuDrawer({ open, onClose, user }) {
       <List onClick={onClose}>
         <ListItem>
           <ListItemButtonLink to="">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
             <ListItemText>Home</ListItemText>
           </ListItemButtonLink>
         </ListItem>
         <ListItem>
           <ListItemButtonLink to="budget">
+            <ListItemIcon>
+              <SavingsIcon />
+            </ListItemIcon>
             <ListItemText>Budgets</ListItemText>
           </ListItemButtonLink>
         </ListItem>
         <ListItem>
           <ListItemButtonLink to="payee">
+            <ListItemIcon>
+              <StoreIcon />
+            </ListItemIcon>
             <ListItemText>Payees</ListItemText>
           </ListItemButtonLink>
         </ListItem>
         <ListItem>
           <ListItemButtonLink to="payment">
+            <ListItemIcon>
+              <PaymentsIcon />
+            </ListItemIcon>
             <ListItemText>Payments</ListItemText>
           </ListItemButtonLink>
         </ListItem>
