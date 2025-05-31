146a1a7057f54d438bddcefd43976041c25c1d4e
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-05-31 10:33:47 +0100

    Remove history state when closed

--- budgetmanagerpwa/src/components/Dialog.tsx
+++ budgetmanagerpwa/src/components/Dialog.tsx
@@ -3,25 +3,24 @@ import React from 'react'
 import { default as MuiDialog } from '@mui/material/Dialog'
 import type { DialogProps } from '@mui/material/Dialog'
 
-type OnClose =
-  | ((
-      event: {},
-      reason: 'backdropClick' | 'escapeKeyDown' | 'backClick'
-    ) => void)
-  | undefined
+type OnClose = (
+  event: {},
+  reason: 'backdropClick' | 'escapeKeyDown' | 'backClick',
+) => void
 
 const Dialog = (props: DialogProps) => {
   const onDialogClose: OnClose = (event, reason) => {
     removeEventListener('popstate', listener)
     if (reason == 'backClick') reason = 'escapeKeyDown'
-    else history.back()
     props.onClose ? props.onClose(event, reason) : null
   }
   const listener = () => onDialogClose({}, 'backClick')
   React.useEffect(() => {
     if (props.open) {
-      history.pushState('#', '')
+      history.pushState({ open: true }, '')
       addEventListener('popstate', listener)
+    } else if (history.state?.open) {
+      history.back()
     }
   }, [props.open])
   return <MuiDialog onClose={onDialogClose} {...props} />
