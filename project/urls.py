0c47bdc5b751376e7a1529c47887aeab9c4f17a0
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-06-26 16:55:32 +0100

    Remove account paths

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,8 @@
 """project URL Configuration"""
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.views import logout_then_login
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('budgetmanager/api/', include('budgetmanager.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/logout/?next=<str:login_url>', logout_then_login, name='logout'),
-    path('accounts/profile/', RedirectView.as_view(url='/budgetmanager/')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
